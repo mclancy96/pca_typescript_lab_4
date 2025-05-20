e4e5fa8b906d852db4a9456a512ad0dbf32c904b
Mike <mclancy96@gmail.com>
2025-05-20 10:17:25 -0400

    Update to distance

--- test/section3_tuples.test.ts
+++ test/section3_tuples.test.ts
@@ -5,21 +5,21 @@ import { join } from "path";
 import vm from "vm";
 
 describe("Lab 4 — Section 3: Tuples", () => {
-let context: any = {};
+  let context: any = {};
 
-before(() => {
-const filePath = join(__dirname, "../src/section3_tuples.ts");
-const tsCode = readFileSync(filePath, "utf8");
-const jsCode = ts.transpile(tsCode);
-vm.createContext(context);
-vm.runInContext(jsCode, context);
-});
+  before(() => {
+    const filePath = join(__dirname, "../src/section3_tuples.ts");
+    const tsCode = readFileSync(filePath, "utf8");
+    const jsCode = ts.transpile(tsCode);
+    vm.createContext(context);
+    vm.runInContext(jsCode, context);
+  });
 
-it("should define 'origin' as a tuple with two numbers", () => {
-const origin = context.origin;
-expect(origin).to.be.an("array");
-expect(origin.length).to.equal(2);
-expect(origin[0]).to.be.a("number");
-expect(origin[1]).to.be.a("number");
-});
+  it("should define 'destination' as a tuple with two numbers", () => {
+    const destination = context.destination;
+    expect(destination).to.be.an("array");
+    expect(destination.length).to.equal(2);
+    expect(destination[0]).to.be.a("number");
+    expect(destination[1]).to.be.a("number");
+  });
 });
